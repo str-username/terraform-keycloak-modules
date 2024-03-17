a1e52855011888559565803df949dd78a3caa5e6
str-username <nikolay.semenov91@gmail.com>
2024-03-18 00:11:39 +0400

    feature(examples/clients/oidc/grafana): create keycloak oidc grafana example client

---
+++ examples/clients/oidc/grafana/main.tf
@@ -0,0 +1,20 @@
+module "grafana_oidc_client" {
+  source                                     = "../../../../modules/clients/oidc"
+  client_id                                  = "grafana-oauth"
+  realm_id                                   = "sandbox"
+  name                                       = "Grafana SSO"
+  description                                = "Monitoring UI solution"
+  display_on_consent_screen                  = false
+  root_url                                   = "https://grafana.host.mydomain.com"
+  base_url                                   = "https://grafana.host.mydomain.com"
+  valid_redirect_uris                        = ["https://grafana.host.mydomain.com/generic_oauth"]
+  web_origins                                = ["https://grafana.host.mydomain.com"]
+  admin_url                                  = "https://grafana.host.mydomain.com"
+  client_authenticator_type                  = "client-secret"
+  standard_flow_enabled                      = true
+  direct_access_grants_enabled               = true
+  consent_required                           = false
+  frontchannel_logout_enabled                = true
+  backchannel_logout_session_required        = true
+  backchannel_logout_revoke_offline_sessions = false
+}
